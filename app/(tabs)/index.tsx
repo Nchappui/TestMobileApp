71d5b6844c16ee64ddf0d4191874d772b2d99be9
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-06-17 11:20:00 +0200

    Update welcome message to include user's name

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,9 +17,10 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome Nolan!</ThemedText>
         <HelloWave />
       </ThemedView>
+      
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
